64e3f46c6e80912de846366ad7cfb05147c238b8
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-02-28 17:43:15 +0000

    added test table

---
+++ Tennis/table.js
@@ -0,0 +1,5 @@
+window.onload = function table() {
+    var myTable = `<table id=main-table> <tr><th>Year</th><th>Tournament</th><th>Winner</th><th>Runner-up</th></tr>`;
+
+    document.getElementById("tableSpace").innerHTML = myTable + '</table>';
+}
