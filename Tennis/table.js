e898758791e90c24ee50d6102b1572d557c5f3a7
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-03-03 22:50:55 +0000

    added some empty forms

--- Tennis/table.js
+++ Tennis/table.js
@@ -1,5 +1,12 @@
 window.onload = function table() {
-    var myTable = `<table id=main-table> <tr><th>Year</th><th>Tournament</th><th>Winner</th><th>Runner-up</th></tr>`;
+    var myTable = `<table id=main-table> 
+                        <tr>
+                            <th>Year</th>
+                            <th>Tournament</th>
+                            <th>Winner</th>
+                            <th>Runner-up</th>
+                        </tr>`;
 
     document.getElementById("tableSpace").innerHTML = myTable + '</table>';
+
 }
