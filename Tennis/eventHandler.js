d89545e5b744aa266d6b2950a4c810a19bdfdb4d
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-03-07 09:26:23 +0000

    added event hndlers and move files around so that JSON worked. Added method to retrieve JSON data

--- Tennis/eventHandler.js
+++ Tennis/eventHandler.js
@@ -1,1 +1,6 @@
 // this should listen for button press and contain the logic to query the JSon files
+
+window.onload = function doEvent() {
+    document.getElementById("submit").addEventListener('click', onClickSubmit)
+    document.getElementById("reset").addEventListener('click', resetTable )
+}
--- Tennis/table.js
+++ Tennis/table.js
@@ -1,5 +1,25 @@
-window.onload = function table() {
+function table() {
     var myTable = `<table id=main-table> <tr><th>Year</th><th>Tournament</th><th>Winner</th><th>Runner-up</th></tr>`;
-
     document.getElementById("tableSpace").innerHTML = myTable + '</table>';
 }
+
+function resetTable(){
+    if (document.getElementById("main-table") != null){
+        document.getElementById('main-table').remove();
+    }
+}
+
+function onClickSubmit() {
+    getJsonTable();
+
+}
+
+function getJsonTable(){
+    // returns a jQ object with JSon File
+    var fileSelect = $('#MaleFemale').val();
+    $.getJSON( fileSelect + '-grand-slam-winners.json', function(result){
+        $.each(result, function(data){
+            JSON.stringify(data)
+        })
+        }
+)}
